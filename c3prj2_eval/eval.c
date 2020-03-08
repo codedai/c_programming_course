6d03b26d902eb4e74356f779ca55f81f324e0355
Coursera Learner <nobody@nowhere.nul>
2020-03-08 07:38:32 +0000

    hh

--- c3prj2_eval/eval.c
+++ c3prj2_eval/eval.c
@@ -20,31 +20,23 @@ int card_ptr_comp(const void * vp1, const void * vp2) {
 
 
 suit_t flush_suit(deck_t * hand) {
-  card_t** card= hand -> cards;
-  card_t card1;
-  card1 = **(card +0);
-  int s,h,d,c;
-  s=h=d=c=0;
-
-
-  for (size_t i=0 ;i< (hand ->n_cards);i++){
-    card1 = **(card +i);
-    switch(card1.suit){
-    case SPADES : {s++;  break;}
-    case HEARTS : { h++;  break;}
-    case DIAMONDS :{ d++ ;  break;}
-    case CLUBS :{c++ ; break;}
+  card_t ** testCardptrs = (*hand).cards;
+  int s, h, d, c;
+  s = h = d = c = 0;
+  for (size_t i = 1; i < hand->n_cards; i++) {
+    switch (testCardptrs[i]->suit) {
+    case SPADES: s++; break;
+    case HEARTS: h++; break;
+    case DIAMONDS: d++; break;
+    case CLUBS: c++; break;
     case NUM_SUITS: break;
-
     }
   }
-
   if (s >= 5) return SPADES;
   if (h >= 5) return HEARTS;
   if (d >= 5) return DIAMONDS;
   if (c >= 5) return CLUBS;
   return NUM_SUITS;
-
 }
 
 unsigned get_largest_element(unsigned * arr, size_t n) {
